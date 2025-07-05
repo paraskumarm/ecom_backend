6fbf84b2ea91027c7efeb34e7dc616fdafe74858
Mahour, Paras <Paraskumar.Mahour@cvent.com>
2025-07-05 20:47:08 +0530

    remove new line

--- backend/settings.py
+++ backend/settings.py
@@ -165,7 +165,6 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 CORS_ORIGIN_ALLOW_ALL = True
 
-
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
